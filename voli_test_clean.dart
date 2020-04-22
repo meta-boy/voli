159deea96869af4e2c1407c4ce71500da6ba1a3f
Source <anurag4884@gmail.com>
2020-04-22 17:41:20 +0530

    Delete voli_test_clean.dart

--- voli_test_clean.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-import 'package:path/path.dart' as path;
-import '../lib/constants/paths.dart';
-
-main(List<String> args) {
-  final dir = Directory(path.join(packageDirectory, 'test', 'dir_test'));
-  dir.deleteSync(recursive: true);
-  Directory(path.join(packageDirectory, 'test', 'dir_test')).create();
-}
